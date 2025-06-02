4fffd8257df3e3eb5a4493cdc3c93ae3fea7e0ea
wesleyberto <wesleyberto4@gmail.com>
2025-06-02 11:53:10 -0300

    fix adicionando componente de informaçoes do repositorio pesquisado

---
+++ src/components/ItemList/index.jsx
@@ -0,0 +1,12 @@
+import './styled.css';
+
+const ItemList = ({ title, description }) => {
+    return (
+        <div className='item-list'>
+            <strong>{title}</strong>
+            <p>{description}</p>
+            <hr />
+        </div>
+    );
+}
+export default ItemList;
